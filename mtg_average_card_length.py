9cd7a8d22682179cec84613a8ecc2670d0e512ba
brooks42 <britainsux@gmail.com>
2025-06-21 21:18:23 -0700

    add special cockatrice file with keyword replacements, add a script to get the average card length of every mtg card to compare to yugioh

--- mtg_average_card_length.py
+++ mtg_average_card_length.py
@@ -4,10 +4,10 @@
 # to run you need to do
 # `pip3 install beautifulsoup4`
 # `pip3 install lxml`
+# `pip3 install xmltodict`
 
 import sys
-
-from bs4 import BeautifulSoup
+import xmltodict
 
 
 def main():
@@ -30,75 +30,28 @@ def main():
         print("Loading file...")
         cube_list = set()
 
-        with open(filename, "r") as f:
-            soup_obj = BeautifulSoup(f, "xml")
+        total_cards = 0
+        combined_length_of_all_card_texts = 0
+
+        with open(filename, "r") as file:
+            # soup_obj = BeautifulSoup(f, "xml")
 
             # grab the list of cards out of the input cockatrice xml file
-            all_cards = soup_obj.findAll("card")
+            # all_cards = soup_obj.findAll("card")
+
+            xml_content = file.read()
+            all_cards = xmltodict.parse(xml_content)["cockatrice_carddatabase"]["cards"]["card"]
 
             for index in range(len(all_cards)):
+                try:
+                    total_cards += 1
+                    combined_length_of_all_card_texts += len(all_cards[index]["text"].split())
+                except:
+                    pass
 
-                # grab the info and transform this into the Card instance format above
-                cockatrice_card = all_cards[index]
-
-                for name_tag in cockatrice_card:
-                    if name_tag.name == 'name':
-                        card_name = name_tag.string
-
-                        exclude = False
-
-                        # these cards aren't supported in dr4ft (and aren't good/fun enough to work around)
-                        if card_name in ["1996 World Champion", "Hazmat Suit (Used)"]:
-                            exclude = True
-
-                        for tag in cockatrice_card:
-                            if tag.name == 'prop':
-                                for type_tag in tag:
-                                    if type_tag.name == 'maintype':
-                                        if 'Scheme' in type_tag.string:
-                                            print(
-                                                f'filtering scheme ${card_name}')
-                                            exclude = True
-                                        if 'Phenomenon' in type_tag.string:
-                                            print(
-                                                f'filtering phenomenon ${card_name}')
-                                            exclude = True
-                                        if 'Vanguard' in type_tag.string:
-                                            print(
-                                                f'filtering vanguard ${card_name}')
-                                            exclude = True
-                                        if 'Hero' in type_tag.string:
-                                            print(
-                                                f'filtering hero ${card_name}')
-                                            exclude = True
-                                        if 'Plane' in type_tag.string:
-                                            print(
-                                                f'filtering plane ${card_name}')
-                                            exclude = True
-
-                                    if type_tag.name == 'type':
-                                        if 'Basic Land' in type_tag.string:
-                                            print(
-                                                f'filtering basic land ${card_name}')
-                                            exclude = True
-
-                        if exclude:
-                            continue
-
-                        # only append non-token rarities, and append twice if rarity is common
-                        for rarity_tag in cockatrice_card:
-                            if rarity_tag.name == 'set':
-
-                                # prune tokens
-                                if rarity_tag['rarity'] == 'token':
-                                    continue
-
-                                cube_list.add(card_name)
-
-        print(f'Writing {len(cube_list)} cards to cube file...')
-        with open('example_cube.txt', 'w') as f:
-            f.write('\n'.join(str(item) for item in cube_list))
-
+        print(f'Total cards: {total_cards}')
+        print(f'Length of all cards: {combined_length_of_all_card_texts}')
+        print(f'Average length: {combined_length_of_all_card_texts / total_cards}')
         print('Done')
 
     except Exception as e:
